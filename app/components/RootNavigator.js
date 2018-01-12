6e8ec947fb85d7da65efe0dff9f0a8c80cc62619
Christopher De Camp <cdecamp77@gmail.com>
2018-01-12 14:15:38 -0800

    Navigation to maint and ops forms

--- app/components/RootNavigator.js
+++ app/components/RootNavigator.js
@@ -10,12 +10,43 @@ import {
   } from 'react-native';  
 import React, { Component } from 'react';
 import IEPManager from '../pages/IEPManager/IEPManager'
+import FRAT from '../pages/FRAT/FRAT'
+import GRAT from '../pages/GRAT/GRAT'
 import Maint591 from '../pages/Maint591/Maint591'
+import Maint691 from '../pages/Maint691/Maint691'
+import Maint791 from '../pages/Maint791/Maint791'
+import Maint4135 from '../pages/Maint4135/Maint4135'
+import Maint5135 from '../pages/Maint5135/Maint5135'
+import Maint7135 from '../pages/Maint7135/Maint7135'
+import Maint8135 from '../pages/Maint8135/Maint8135'
+import Ops8RW from '../pages/Ops8RW/Ops8RW'
+import Ops591 from '../pages/Ops591/Ops591'
+import Ops791 from '../pages/Ops791/Ops791'
+import Ops1135 from '../pages/Ops1135/Ops1135'
+import Ops4135 from '../pages/Ops4135/Ops4135'
+import Ops5135 from '../pages/Ops5135/Ops5135'
+import Ops6135 from '../pages/Ops6135/Ops6135'
+import Ops7135 from '../pages/Ops7135/Ops7135'
 
 const RootNavigator = StackNavigator({
     IEPManager: {screen: IEPManager},
+    FRAT: {screen: FRAT},
+    GRAT: {screen: GRAT},
     Maint591: {screen: Maint591},
-
+    Maint691: {screen: Maint691},
+    Maint791: {screen: Maint791},
+    Maint4135: {screen: Maint4135},
+    Maint5135: {screen: Maint5135},
+    Maint7135: {screen: Maint7135},
+    Maint8135: {screen: Maint8135},
+    Ops8RW: {screen: Ops8RW},
+    Ops591: {screen: Ops591},
+    Ops791: {screen: Ops791},
+    Ops1135: {screen: Ops1135},
+    Ops4135: {screen: Ops4135},
+    Ops5135: {screen: Ops5135},
+    Ops6135: {screen: Ops6135},
+    Ops7135: {screen: Ops7135},
   });
 
 export default RootNavigator;
--- app/pages/IEPManager/IEPManager.js
+++ app/pages/IEPManager/IEPManager.js
@@ -13,113 +13,64 @@ export default class IEPManager extends Component {
             flex: 1,
             backgroundColor: '#ffffff'
             }}>
-        <Button
-            title="New Form"
-            color="#841584"
-            onPress={() =>
-                navigate('Maint591')
-            }
-        />
-        <View style={styles.container}>
+            
             <Text style={styles.h1}>Internal Evaluation Program Manager</Text>
             <Text style={styles.newChecklist}>New Checklists</Text>
-            <View style={styles.newForms}>
-            <Button title="USFS"
-            color="#841584"
-            onPress={() =>
-                navigate('Maint591')
-            }>
-                
+            
+            <Button onPress={() => navigate('Ops8RW')} title="OPS 8-(RW) Aviation Life Support / Survival Equipment (2016)">
             </Button>
-            <Button onPress={this._onPressButton} title="OPS 8-(RW) Aviation Life Support / Survival Equipment (2016)">
+
+            <Button onPress={() => navigate('Ops8135')} title="Ops 8- Cabin Crew and Service Reps Part 135 (2016)">
             </Button>
-            <Button onPress={this._onPressButton} title="">
-                <View style={styles.button}>
-                    <Text style={styles.buttonText}>Ops 8- Cabin Crew and Service Reps Part 135 (2016)</Text>
-                </View>
+
+            <Button onPress={() => navigate('Ops791')} title="Ops 7- Flight Standards 2017 (Part 91)">
             </Button>
-            <Button onPress={this._onPressButton} title="">
-                <View style={styles.button}>
-                    <Text style={styles.buttonText}>Ops 7- Flight Standards 2017 (Part 91)</Text>
-                </View>
+
+            <Button onPress={() => navigate('Ops7135')} title="Ops 7- Flight Standards 2017 (Part 135)">
             </Button>
-            <Button onPress={this._onPressButton} title="">
-                <View style={styles.button}>
-                    <Text style={styles.buttonText}>Ops 7- Flight Standards 2017 (Part 135)</Text>
-                </View>
+
+            <Button onPress={() => navigate('Ops591')} title="Ops 5- Pilot Hiring 2017 (part 91)">
             </Button>
-            <Button onPress={this._onPressButton} title="">
-                <View style={styles.button}>
-                    <Text style={styles.buttonText}>Ops 5- Pilot Hiring 2017 (part 91)</Text>
-                </View>
+
+            <Button onPress={() => navigate('Ops5135')} title="Ops 5- Pilot Hiring 2017 (part 135)">
             </Button>
-            <Button onPress={this._onPressButton} title="">
-                <View style={styles.button}>
-                    <Text style={styles.buttonText}>Ops 5- Pilot Hiring 2017 (part 135)</Text>
-                </View>
+
+            <Button onPress={() => navigate('Ops1135')} title="Ops 1- Operations Management 2016 (Part 135)">
             </Button>
-            <Button onPress={this._onPressButton} title="">
-                <View style={styles.button}>
-                    <Text style={styles.buttonText}>Ops 1- Operations Management 2016 (Part 135)</Text>
-                </View>
+
+            <Button onPress={() => navigate('Ops691')} title="Operations 6- Pilot Training Part 91 (2017)">
             </Button>
-            <Button onPress={this._onPressButton} title="">
-                <View style={styles.button}>
-                    <Text style={styles.buttonText}>Operations 6- Pilot Training Part 91 (2017)</Text>
-                </View>
+
+            <Button onPress={() => navigate('Ops6135')} title="Operations 6- Pilot Training Part 135 (2017)">
             </Button>
-            <Button onPress={this._onPressButton} title="">
-                <View style={styles.button}>
-                    <Text style={styles.buttonText}>Operations 6- Pilot Training Part 135 (2017)</Text>
-                </View>
+
+            <Button onPress={() => navigate('Maint691')} title="Maintenance 6- Control/Planning Part 91 (2017)">
             </Button>
-            <Button onPress={this._onPressButton} title="">
-                <View style={styles.button}>
-                    <Text style={styles.buttonText}>Maintenance 6- Control/Planning Part 91 (2017)</Text>
-                </View>
+
+            <Button onPress={() => navigate('Maint591')} title="Maintenance 5- Training 2017 (Part 91)">
             </Button>
-            <Button onPress={this._onPressButton} title="">
-                <View style={styles.button}>
-                    <Text style={styles.buttonText}>Maintenance 5- Training 2017 (Part 91)</Text>
-                </View>
+
+            <Button onPress={() => navigate('Maint5135')} title="">
             </Button>
-            <Button onPress={this._onPressButton} title="">
-                <View style={styles.button}>
-                    <Text style={styles.buttonText}>Maintenance 5- Training 2017 (Part 135)</Text>
-                </View>
+
+            <Button onPress={() => navigate('Maint4135')} title="Maintenance 4- Inspection Program Part 135 (2017)">
             </Button>
-            <Button onPress={this._onPressButton} title="">
-                <View style={styles.button}>
-                    <Text style={styles.buttonText}>Maintenance 4- Inspection Program Part 135 (2017)</Text>
-                </View>
+
+            <Button onPress={() => navigate('Maint11')} title="Maintenance 11- Hangar/Facilities (2016)">
             </Button>
-            <Button onPress={this._onPressButton} title="">
-                <View style={styles.button}>
-                    <Text style={styles.buttonText}>Maintenance 11- Hangar/Facilities (2016)</Text>
-                </View>
+
+            <Button onPress={() => navigate('Maint8135')} title="Maintenance 8- Records 2016 (Part 135)">
             </Button>
-            <Button onPress={this._onPressButton} title="">
-                <View style={styles.button}>
-                    <Text style={styles.buttonText}>Maint 8- Records 2016 (Part 135)</Text>
-                </View>
+
+            <Button onPress={() => navigate('Maint791')} title="Maintenance 7- Aircraft Condition 2017 (Part 91)">
             </Button>
-            <Button onPress={this._onPressButton} title="">
-                <View style={styles.button}>
-                    <Text style={styles.buttonText}>Maint 7- Aircraft Condition 2017 (Part 91)</Text>
-                </View>
+
+            <Button onPress={() => navigate('Maint7135')} title="Maintenance 7- Aircraft Condition 2017 (Part 135)">
             </Button>
-            <Button onPress={this._onPressButton} title="">
-                <View style={styles.button}>
-                    <Text style={styles.buttonText}>Maint 7- Aircraft Condition 2017 (Part 135)</Text>
-                </View>
+            
+            <Button onPress={() => navigate('Maint6135')} title="Maint 6- Control/Planning Part 135 (2017)">
             </Button>
-            <Button onPress={this._onPressButton} title="">
-                <View style={styles.button}>
-                    <Text style={styles.buttonText}>Maint 6- Control/Planning Part 135 (2017)</Text>
-                </View>
-            </Button>
-            </View>
-        </View>
+
       </ScrollView>
     );
   }
